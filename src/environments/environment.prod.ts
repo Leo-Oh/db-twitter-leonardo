217053d8cb2ce297edbd8331e6df3b5f5571da10
xXLeonardoMartinezXX <leonardo.m2349@gmail.com>
2020-12-23 17:55:39 -0600

    Firebase config and tweets marketing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseCOnfig:{
+    apiKey: "AIzaSyDLDmDw2xcxA6tUQUllyd_qW88GFi0NcXM",
+    authDomain: "tweeter-leonardo.firebaseapp.com",
+    projectId: "tweeter-leonardo",
+    storageBucket: "tweeter-leonardo.appspot.com",
+    messagingSenderId: "121484188848",
+    appId: "1:121484188848:web:24e39861391e126fb1af75",
+    measurementId: "G-7VPEFYX6KZ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseCOnfig:{
+    apiKey: "AIzaSyDLDmDw2xcxA6tUQUllyd_qW88GFi0NcXM",
+    authDomain: "tweeter-leonardo.firebaseapp.com",
+    projectId: "tweeter-leonardo",
+    storageBucket: "tweeter-leonardo.appspot.com",
+    messagingSenderId: "121484188848",
+    appId: "1:121484188848:web:24e39861391e126fb1af75",
+    measurementId: "G-7VPEFYX6KZ"
+  }
 };
 
 /*
